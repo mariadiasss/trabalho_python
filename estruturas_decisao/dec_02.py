8551d660487151cdd0b9981f2b2947bb195ae259
mariadiasss <mariacoelhodias94@gmail.com>
2024-11-28 10:25:12 -0300

    Operadores

--- estruturas_decisao/dec_02.py
+++
@@ -1,9 +0,0 @@
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
-if numero1 > numero2:
-    print(f"O maior número é {numero1}.")
-elif numero1 < numero2:
-    print(f"O maior número é {numero2}.")
-else:
-    print("Os números são iguais.")
-    
