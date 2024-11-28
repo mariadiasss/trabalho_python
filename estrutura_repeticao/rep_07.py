4cd2ccb8ee7e058799d71e8e6a2d66c4bad0dd49
mariadiasss <mariacoelhodias94@gmail.com>
2024-11-28 09:27:20 -0300

    Delete estrutura_repeticao directory

--- estrutura_repeticao/rep_07.py
+++
@@ -1,9 +0,0 @@
-inicio = int(input("Digite o início do intervalo: "))
-fim = int(input("Digite o fim do intervalo: "))
-
-if inicio > fim:
-    print("Erro: o início não pode ser maior que o fim.")
-else:
-    for i in range(inicio, fim + 1):
-        if i % 2 == 0:
-            print(i)
