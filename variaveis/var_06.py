e61650d2a12262b02422c7c3eeb627ded48899df
mariadiasss <mariacoelhodias94@gmail.com>
2024-11-28 09:27:37 -0300

    Delete variaveis directory

--- variaveis/var_06.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Digite o valor de a: "))
-b = int(input("Digite o valor de b: "))
-c = int(input("Digite o valor de c: "))
-
-# Troca de valores
-a, b, c = b, c, a
-
-print(f"Novo valor de a: {a}, novo valor de b: {b}, novo valor de c: {c}")
