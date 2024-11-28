14dc1cbdc8b515add89bef2cdd9dc1d04dcd0498
mariadiasss <mariacoelhodias94@gmail.com>
2024-11-28 10:22:24 -0300

    Delete estruturas_decisao directory

--- estruturas_decisao/dec_01.py
+++
@@ -1,7 +0,0 @@
-numero = int(input("Digite um número inteiro: "))
-if numero > 0:
-    print("O número é positivo.")
-elif numero < 0:
-    print("O número é negativo.")
-else:
-    print("O número é zero.")
